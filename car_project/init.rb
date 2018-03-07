c7515ff18add89ee6c58ac4b25729960bb6fd4c5
txusko <txusko@gmail.com>
2018-03-07 14:00:53 +0100

    Change car_project folder with the latest version

--- car_project/init.rb
+++ car_project/init.rb
@@ -1,20 +1,25 @@
+#!/usr/bin/env ruby
+
 require_relative('lib/car')
 
 puts
 puts "Enter information for the car you want to purchase."
 puts
 
-puts "Make: "
+print "Make: "
 make = gets.chomp
 
-puts "Year: "
+print "Year: "
 year = gets.chomp
 
-puts "Color: "
+print "Color: "
 color = gets.chomp
 
-car = Car.new(:make => make, :year => year, :color => color)
+print "Doors: "
+doors = gets.chomp
+
+car = Car.new(:make => make, :year => year, :color => color, :doors => doors)
 
 puts
 puts "I understand that you want to purchase: #{car.full_name}."
-puts 
+puts
--- car_project/spec/expectation_matchers_spec.rb
+++ car_project/spec/expectation_matchers_spec.rb
@@ -1,5 +1,5 @@
 describe 'Expectation Matchers' do
-
+  
   describe 'equivalence matchers' do
 
     it 'will match loose equality with #eq' do
@@ -12,7 +12,7 @@ describe 'Expectation Matchers' do
       d = 17.0
       expect(c).to eq(d)        # different types, but "close enough"
     end
-
+    
     it 'will match value equality with #eql' do
       a = "2 cats"
       b = "2 cats"
@@ -32,9 +32,9 @@ describe 'Expectation Matchers' do
       expect(b).to equal(c)     # same object
       expect(b).to be(c)        # synonym for #equal
     end
-
+    
   end
-
+  
   describe 'truthiness matchers' do
 
     it 'will match true/false' do
@@ -45,7 +45,7 @@ describe 'Expectation Matchers' do
       expect(nil).not_to be(false)    # nil is not exactly false
       expect(0).not_to be(false)      # 0 is not exactly false
     end
-
+    
     it 'will match truthy/falsey' do
       expect(1 < 2).to be_truthy
       expect(1 > 2).to be_falsey
@@ -54,19 +54,19 @@ describe 'Expectation Matchers' do
       expect(nil).to be_falsey        # nil counts as false
       expect(0).not_to be_falsey      # but 0 is still not falsey enough
     end
-
+    
     it 'will match nil' do
       expect(nil).to be_nil
       expect(nil).to be(nil)          # either way works
-
+      
       expect(false).not_to be_nil     # nil only, just like #nil?
       expect(0).not_to be_nil         # nil only, just like #nil?
     end
 
   end
-
+  
   describe 'numeric comparison matchers' do
-
+  
     it 'will match less than/greater than' do
       expect(10).to be > 9
       expect(10).to be >= 10
@@ -82,18 +82,18 @@ describe 'Expectation Matchers' do
     end
 
   end
-
+  
   describe 'collection matchers' do
-
+    
     it 'will match arrays' do
       array = [1,2,3]
-
+      
       expect(array).to include(3)
       expect(array).to include(1,3)
 
       expect(array).to start_with(1)
       expect(array).to end_with(3)
-
+      
       expect(array).to match_array([3,2,1])
       expect(array).not_to match_array([1,2])
 
@@ -103,30 +103,30 @@ describe 'Expectation Matchers' do
 
     it 'will match strings' do
       string = 'some string'
-
+      
       expect(string).to include('ring')
       expect(string).to include('so', 'ring')
-
+      
       expect(string).to start_with('so')
       expect(string).to end_with('ring')
     end
-
+    
     it 'will match hashes' do
       hash = {:a => 1, :b => 2, :c => 3}
-
+      
       expect(hash).to include(:a)
       expect(hash).to include(:a => 1)
-
+      
       expect(hash).to include(:a => 1, :c => 3)
       expect(hash).to include({:a => 1, :c => 3})
-
-      expect(hash).not_to include({'a' => 1, 'c' => 3}) #string not always going to match
+      
+      expect(hash).not_to include({'a' => 1, 'c' => 3})
     end
-
+    
   end
-
+  
   describe 'other useful matchers' do
-
+  
     it 'will match strings with a regex' do
       # This matcher is a good way to "spot check" strings
       string = 'The order has been received.'
@@ -148,7 +148,7 @@ describe 'Expectation Matchers' do
       expect('test').to be_a(String)          # alias of #be_kind_of
       expect([1,2,3]).to be_an(Array)         # alias of #be_kind_of
     end
-
+    
     it 'will match objects with #respond_to' do
       string = 'test'
       expect(string).to respond_to(:length)
@@ -169,7 +169,7 @@ describe 'Expectation Matchers' do
         :make => 'Dodge', :year => 2010, :color => 'green'
       )
     end
-
+    
     it 'will match anything with #satisfy' do
       # This is the most flexible matcher
       expect(10).to satisfy do |value|
@@ -178,12 +178,12 @@ describe 'Expectation Matchers' do
     end
 
   end
-
+  
   describe 'predicate matchers' do
-
+    
     it 'will match be_* to custom methods ending in ?' do
       # drops "be_", adds "?" to end, calls method on object
-      # Can use these when methods end in "?", require no arguments,
+      # Can use these when methods end in "?", require no arguments, 
       # and return true/false.
 
       # with built-in methods
@@ -193,9 +193,9 @@ describe 'Expectation Matchers' do
       expect(1).to be_nonzero               # 1.nonzero?
       expect(1).to be_odd                   # 1.odd?
       expect(2).to be_even                  # 1.even?
-
+      
       # be_nil is actually an example of this too
-
+      
       # with custom methods
       class Product
         def visible?; true; end
@@ -208,7 +208,7 @@ describe 'Expectation Matchers' do
 
     it 'will match have_* to custom methods like has_*?' do
       # changes "have_" to "has_", adds "?" to end, calls method on object
-      # Can use these when methods start with "has_", end in "?",
+      # Can use these when methods start with "has_", end in "?", 
       # and return true/false. Can have arguments, but not required.
 
       # with built-in methods
@@ -225,16 +225,16 @@ describe 'Expectation Matchers' do
       expect(customer).to have_pending_order  # customer.has_pending_order?
       expect(customer.has_pending_order?).to be true  # same as this
     end
-
+    
   end
-
+  
   describe 'observation matchers' do
     # Note that all of these use "expect {}", not "expect()".
-    # It is a special block format that allows a
+    # It is a special block format that allows a 
     # process to take place inside of the expectation.
-
+    
     it 'will match when events change object attributes' do
-      # calls the test before the block,
+      # calls the test before the block, 
       # then again after the block
       array = []
       expect { array << 1 }.to change(array, :empty?).from(true).to(false)
@@ -247,20 +247,20 @@ describe 'Expectation Matchers' do
       hits = WebsiteHits.new
       expect { hits.increment }.to change(hits, :count).from(0).to(1)
     end
-
+    
     it 'will match when events change any values' do
-      # calls the test before the block,
+      # calls the test before the block, 
       # then again after the block
 
-      # notice the "{}" after "change",
+      # notice the "{}" after "change", 
       # can be used on simple variables
       x = 10
       expect { x += 1 }.to change {x}.from(10).to(11)
       expect { x += 1 }.to change {x}.by(1)
       expect { x += 1 }.to change {x}.by_at_least(1)
       expect { x += 1 }.to change {x}.by_at_most(1)
-
-      # notice the "{}" after "change",
+      
+      # notice the "{}" after "change", 
       # can contain any block of code
       z = 11
       expect { z += 1 }.to change { z % 3 }.from(2).to(0)
@@ -268,22 +268,22 @@ describe 'Expectation Matchers' do
       # Must have a value before the block
       # Must change the value inside the block
     end
-
+    
     it 'will match when errors are raised' do
       # observes any errors raised by the block
-
+      
       expect { raise StandardError }.to raise_error
       expect { raise StandardError }.to raise_exception
 
       expect { 1 / 0 }.to raise_error(ZeroDivisionError)
       expect { 1 / 0 }.to raise_error.with_message("divided by 0")
       expect { 1 / 0 }.to raise_error.with_message(/divided/)
-
-      # Note that the negative form does
+      
+      # Note that the negative form does 
       # not accept arguments
       expect { 1 / 1 }.not_to raise_error
     end
-
+    
     it 'will match when output is generated' do
       # observes output sent to $stdout or $stderr
 
@@ -293,7 +293,76 @@ describe 'Expectation Matchers' do
 
       expect { warn('problem') }.to output(/problem/).to_stderr
     end
+    
+  end
+  
+  describe 'compound expectations' do
+    
+    it 'will match using: and, or, &, |' do
+      expect([1,2,3,4]).to start_with(1).and end_with(4)
+      
+      expect([1,2,3,4]).to start_with(1) & include(2)
 
+      expect(10 * 10).to be_odd.or be > 50
+      
+      array = ['hello', 'goodbye'].shuffle
+      expect(array.first).to eq("hello") | eq("goodbye")
+    end
+    
+  end
+  
+  describe 'composing matchers' do
+    # some matchers accept matchers as arguments. (new in rspec3)
+    
+    it 'will match all collection elements using a matcher' do
+      array = [1,2,3]
+      expect(array).to all( be < 5 )
+    end
+    
+    it 'will match by sending matchers as arguments to matchers' do
+      string = "hello"
+      expect { string = "goodbye" }.to change { string }.
+        from( match(/ll/) ).to( match(/oo/) )
+
+      hash = {:a => 1, :b => 2, :c => 3}
+      expect(hash).to include(:a => be_odd, :b => be_even, :c => be_odd)
+      expect(hash).to include(:a => be > 0, :b => be_within(2).of(4))
+    end
+    
+    it 'will match using noun-phrase aliases for matchers' do
+      # These are built-in aliases that make 
+      # specs read better by using noun-based 
+      # phrases instead of verb-based phrases.
+      
+      # valid but awkward example
+      fruits = ['apple', 'banana', 'cherry']
+      expect(fruits).to start_with( start_with('a') ) & 
+        include( match(/a.a.a/) ) & 
+        end_with( end_with('y') )
+      
+      # improved version of the previous example
+      # "start_with" becomes "a_string_starting_with"
+      # "end_with" becomes "a_string_ending_with"
+      # "match" becomes "a_string_matching"
+      fruits = ['apple', 'banana', 'cherry']
+      expect(fruits).to start_with( a_string_starting_with('a') ) & 
+        include( a_string_matching(/a.a.a/) ) & 
+        end_with( a_string_ending_with('y') )
+
+
+      # valid but awkward example
+      array = [1,2,3,4]
+      expect(array).to start_with( be <= 2 ) |
+        end_with( be_within(1).of(5) )
+
+      # improved version of the previous example
+      # "be <= 2" becomes "a_value <= 2"
+      # "be_within" becomes "a_value_within"
+      array = [1,2,3,4]
+      expect(array).to start_with( a_value <= 2 ) |
+        end_with( a_value_within(1).of(5) )
+    end
+    
   end
 
 end
